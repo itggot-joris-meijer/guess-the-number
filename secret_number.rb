51237696e62fc4f8b3ee5991300839111b6b9ee5
itggot-joris-meijer <joris.meijer@itggot.se>
2017-03-08 09:35:06 +0100

    la till lite dålig kod

---
+++ secret_number.rb
@@ -0,0 +1,12 @@
+iftruethenrun = true
+secret_number = []
+guessed_ number =
+
+def guess_the_number
+while iftruethenrun
+  puts "Choose difficulty"
+    if difficulty =
+      def generate_secret_number (length:1)
+        secret_number << rand(100)+1
+    end
+end
